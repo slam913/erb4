afd555ed77a1118a4e57aa6cdb0cdf68c31334a8
shirley.lam <shirley.my@gmail.com>
2024-12-20 17:32:46 +0800

    Step 6 listing admin

--- bcre/settings.py
+++ bcre/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "pages.apps.PagesConfig",
+    'listings.apps.ListingsConfig',
+    'realtors.apps.RealtorsConfig'
+
 ]
 
 MIDDLEWARE = [
@@ -77,8 +80,11 @@ WSGI_APPLICATION = 'bcre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':  'bcredb',
+        'USER':  'postgres',
+        'PASSWORD': '1234',
+        'HOST':'localhost'
     }
 }
 
@@ -127,3 +133,10 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+
+
+
